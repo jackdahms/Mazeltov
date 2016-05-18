b60ef95640d6531e60ad2c90cb388779d582fb19
jack <jack.dahms@gmail.com>
2016-05-18 17:57:37 -0500

    created gridpane

--- src/com/jackdahms/Mazeltov.java
+++ src/com/jackdahms/Mazeltov.java
@@ -1,5 +1,23 @@
 package com.jackdahms;
 
-public class Mazeltov {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Mazeltov extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void start(Stage stage) {
+		GridPane grid = new GridPane();
+		
+		stage.setTitle("Mazeltov");
+		stage.setScene(new Scene(new Group(), 1000, 600));
+		stage.show();
+	}
 
 }
