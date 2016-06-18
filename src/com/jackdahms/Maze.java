f3659b2ed529e315511bdbd251b4c2c19693e4cd
jack <jack.dahms@gmail.com>
2016-06-18 00:08:17 -0500

    added wall thickness

--- src/com/jackdahms/Maze.java
+++ src/com/jackdahms/Maze.java
@@ -7,40 +7,134 @@ import javafx.scene.paint.Color;
 public class Maze {
 	
 	int width, height;
+	int imageWidth, imageHeight;
+	int wallThickness;
 		
-	int[][] map; 
+	int[][] horizontalWalls;
+	int[][] verticalWalls;
+	int[][] cells;
 	
 	WritableImage image;
 	PixelWriter writer;
 	
 	/**
 	 * Create a new maze.
-	 * @param width		width of cave in pixels
-	 * @param height	height of cave in pixels
+	 * @param width			number of cells wide to make maze
+	 * @param height		number of cells high to make maze
+	 * @param imageWidth	width in pixels of image
+	 * @param imageHeight	height in pixels of image
 	 */
-	public Maze(int width, int height) {
+	public Maze(int width, int height, int imageWidth, int imageHeight) {
 		this.width = width;
 		this.height = height;
+		this.imageWidth = imageWidth;
+		this.imageHeight = imageHeight;
+		
 		image = new WritableImage(width, height);
 		
-		map = new int[height][width]; //[rows][columns]
+		//both initialized to zero
+		cells = new int[height][width]; //[rows][columns]
+		horizontalWalls = new int[height - 1][width]; //subtract one to account for border
+		verticalWalls = new int[height][width - 1];
+		
+		//so we set everything to one
+		for (int i = 0; i < height - 1; i++) {
+			for (int k = 0; k < width - 1; k++) {
+				horizontalWalls[i][k] = 1;
+				verticalWalls[i][k] = 1;
+			}
+		}
+		for (int i = 0; i < height - 1; i++) {
+			horizontalWalls[i][width - 1] = 1;
+		}
+		for (int k = 0; k < width - 1; k++) {
+			verticalWalls[height - 1][k] = 1;
+		}
+		
+		//test zig zag
+		verticalWalls[0][0] = 0;
+		horizontalWalls[0][1] = 0;
+		horizontalWalls[1][1] = 0;
+		verticalWalls[2][1] = 0;
 	}
 		
 	public void mapToImage() {
-		image = new WritableImage(width, height);
+		image = new WritableImage(imageWidth, imageHeight);
 		writer = image.getPixelWriter();
 		
-		for (int i = 0; i < width / 2; i++) {
-			for (int k = 0; k < height / 2; k++) {
-				writer.setColor(i, k, Color.BLACK);
+		double cellWidth = (double)imageWidth / (double)width;
+		double cellHeight = (double)imageHeight / (double)height;
+		
+		//draw north and south borders. divide by 2 add one to draw half of wall
+		for (int y = 0; y < wallThickness / 2 + 1; y++) {
+			for (int x = 0; x < imageWidth; x++) {
+				writer.setColor(x, y, Color.BLACK);
+				writer.setColor(x, imageHeight - 1 - y, Color.BLACK);
+			}
+		}
+		//draw east and west borders. divide by 2 add one to draw half of wall
+		for (int x = 0; x < wallThickness / 2 + 1; x++) {
+			for (int y = 0; y < imageHeight; y++) {
+				writer.setColor(x, y, Color.BLACK);
+				writer.setColor(imageWidth - 1 - x, y, Color.BLACK);
 			}
 		}
 		
-		for (int i = width / 2; i < width; i++) {
-			for (int k = height / 2; k < height; k++) {
-				writer.setColor(i, k, Color.BLACK);
+		//for each row of horizontal walls
+		for (int i = 0; i < horizontalWalls.length; i++) {
+			//y values to draw at based on row
+			int y1 = (int)((i + 1) * cellHeight - wallThickness / 2); //top of the wall
+			int y2 = (int)((i + 1) * cellHeight + wallThickness / 2); //bottom of the wall
+			//for each horizontal wall within the row
+			for (int k = 0; k < horizontalWalls[i].length; k++) {
+				//if the wall is valid
+				if (horizontalWalls[i][k] == 1) {
+					//x position to start drawing wall at
+					int x = (int)(k * cellWidth);
+					int x1 = x - wallThickness / 2;
+					int x2 = x+(int) (cellWidth + wallThickness / 2); 
+					//w for width! add one to the loop so it draws the same thickness as inputed
+					for (int w = y1; w < y2 + 1; w++) {
+						//l for length!
+						for (int l = x1; l < x2 + 1; l++) {
+							try {
+								writer.setColor(l, w, Color.BLACK);
+							} catch (Exception e) {/*in case walls spill over border*/}
+						}
+					}
+					
+				}
 			}
 		}
+		
+		//for each row of vertical walls
+		for (int row = 0; row < verticalWalls.length; row++) {
+			//y value based on row
+			int y = (int)(row * cellHeight);
+			//for each vertical wall within the row
+			for (int column = 0; column < verticalWalls[row].length; column++) {
+				//if the wall is valid
+				if (verticalWalls[row][column] == 1) {
+					int x1 = (int)((column + 1) * cellWidth - wallThickness / 2); //left of the wall
+					int x2 = (int)((column + 1) * cellWidth + wallThickness / 2); //right of the wall
+					//w for width! add one to the loop so it draws the same thickness as inputed
+					for (int w = x1; w < x2 + 1; w++) {
+						for (int length = 0; length < cellHeight; length++) {
+							try {
+								writer.setColor(w, y + length, Color.BLACK);
+							} catch (Exception e) {/*in case wall spills over border*/}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public void resize(int imageWidth, int imageHeight) {
+		this.imageWidth = imageWidth;
+		this.imageHeight = imageHeight;
+		
+		mapToImage();
 	}
 
 }
--- src/com/jackdahms/Mazeltov.java
+++ src/com/jackdahms/Mazeltov.java
@@ -4,37 +4,31 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
-import javafx.scene.image.Image;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Mazeltov extends Application {
 	
-	int SCENE_WIDTH = 1000;
-	int SCENE_HEIGHT = 600;
+	static int SCENE_WIDTH = 1000;
+	static int SCENE_HEIGHT = 600;
 	
-	int width = 400;
-	int height = 200;
+	static int width = 40;
+	static int height = 30;
+	static int wallThickness = 6;
 	
 	static Maze maze;
 	
-	Canvas canvas;
+	static Canvas canvas;
 	GraphicsContext g;
 	
 	private void repaint(long currentNanoTime) {
@@ -43,7 +37,8 @@ public class Mazeltov extends Application {
 	}
 
 	public static void main(String[] args) {
-		maze = new Maze(400, 200);
+		maze = new Maze(width, height, SCENE_WIDTH - 200, SCENE_HEIGHT);
+		maze.wallThickness = wallThickness;
 		maze.mapToImage();
 		launch(args);
 	}
@@ -51,7 +46,7 @@ public class Mazeltov extends Application {
 	public void start(Stage stage) {
 		GridPane pane = new GridPane();
 		pane.setAlignment(Pos.TOP_RIGHT);
-		canvas = new Canvas(800, 600);
+		canvas = new Canvas(SCENE_WIDTH - 200, SCENE_HEIGHT);
 		g = canvas.getGraphicsContext2D();
 		GridPane.setHgrow(canvas, Priority.ALWAYS);
 		GridPane.setVgrow(canvas, Priority.ALWAYS);
@@ -67,30 +62,34 @@ public class Mazeltov extends Application {
 		controlGrid.setHgap(5);
 		controlGrid.setVgap(5);
 		controlGrid.setPadding(new Insets(5, 5, 5, 5));
-		ColumnConstraints cc = new ColumnConstraints();
-		cc.setMinWidth(100);
-		controlGrid.getColumnConstraints().add(cc); //apply constraint to first column
-		controlGrid.getColumnConstraints().add(cc); //apply constraint to second column
+		controlGrid.setMinWidth(180);
 		pane.add(controlGrid, 1, 0);
 		
 		Label widthLabel = new Label("WIDTH");
-		widthLabel.setPrefWidth(100);
+		widthLabel.setMinWidth(90);
 		controlGrid.add(widthLabel, 0, 0);
 		
-		Spinner<Integer> widthSpinner = new Spinner<Integer>(1, 8000, 400, 100); //TODO decide on width min/max
+		Spinner<Integer> widthSpinner = new Spinner<Integer>(1, 8000, width, 100); //TODO decide on width min/max
 		widthSpinner.setEditable(true);
-		widthSpinner.setPrefWidth(100);
 		widthSpinner.valueProperty().addListener(observable -> width = widthSpinner.getValue());
 		controlGrid.add(widthSpinner, 1, 0);
 
 		Label heightLabel = new Label("HEIGHT");
 		controlGrid.add(heightLabel, 0, 1);
 
-		Spinner<Integer> heightSpinner = new Spinner<Integer>(1, 8000, 200, 100); //TODO decide on height min/max 
+		Spinner<Integer> heightSpinner = new Spinner<Integer>(1, 8000, height, 100); //TODO decide on height min/max 
 		heightSpinner.setEditable(true);
-		heightSpinner.setPrefWidth(100);
 		heightSpinner.valueProperty().addListener(observable -> height = heightSpinner.getValue());
 		controlGrid.add(heightSpinner, 1, 1);
+		
+		Label wallThicknessLabel = new Label("WALL THICKNESS");
+		controlGrid.add(wallThicknessLabel, 0, 2);
+		
+		Spinner<Integer> wallThicknessSpinner = new Spinner<Integer>(1, 10, wallThickness, 1);
+		wallThicknessSpinner.setEditable(true);
+		wallThicknessSpinner.valueProperty().addListener(observable -> wallThickness = wallThicknessSpinner.getValue());
+		wallThicknessSpinner.setMaxWidth(70);
+		controlGrid.add(wallThicknessSpinner, 1, 2);
 		
 		Scene scene = new Scene(pane, SCENE_WIDTH, SCENE_HEIGHT);
 		controlGrid.requestFocus(); //must be done after scene is constructed
@@ -99,16 +98,21 @@ public class Mazeltov extends Application {
 		} catch (Exception e) {
 			System.err.println("Could not load stylesheet...");
 		}
+		
 		InvalidationListener sizeListener = new InvalidationListener() {
 			public void invalidated(Observable arg0) {
 				pane.getChildren().remove(canvas);
 				canvas = new Canvas(scene.getWidth() - 200, scene.getHeight());
 				g = canvas.getGraphicsContext2D();
 				pane.add(canvas, 0, 0);
+				try {
+					maze.resize((int)canvas.getWidth(), (int)canvas.getHeight());
+				} catch (Exception e) {System.err.println("error in redrawing maze");}
 				repaint(0);
 				
 			}			
 		};
+		
 		//could have used lambdas here but I didn't want to retype the exact same code
 		scene.widthProperty().addListener(sizeListener);
 		scene.heightProperty().addListener(sizeListener);
