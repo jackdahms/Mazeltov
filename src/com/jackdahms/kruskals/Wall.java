ecce39919b42f634b0bc93d3581b970726f7d37f
jackdahms <jack.dahms@gmail.com>
2016-07-03 14:25:58 -0500

    created Kruskal's wall class

---
+++ src/com/jackdahms/kruskals/Wall.java
@@ -0,0 +1,14 @@
+package com.jackdahms.kruskals;
+
+public class Wall {
+	
+	int row, col;
+	boolean horizontal;
+	
+	Wall(int row, int col, boolean horizontal) {
+		this.row = row;
+		this.col = col;
+		this.horizontal = horizontal;
+	}
+
+}
