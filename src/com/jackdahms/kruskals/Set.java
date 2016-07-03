a2587505c934f8273a83db1db8143a5684dea4ba
jackdahms <jack.dahms@gmail.com>
2016-07-03 15:41:24 -0500

    merge no longer returns a set. now actually merges the two into one

--- src/com/jackdahms/kruskals/Set.java
+++ src/com/jackdahms/kruskals/Set.java
@@ -4,6 +4,10 @@ public class Set {
 
 	Cell[] cells;
 	
+	Set() {
+		cells = new Cell[1];
+	}
+	
 	Set(Cell first) {
 		cells = new Cell[1];
 		cells[0] = first;
@@ -13,25 +17,30 @@ public class Set {
 		this.cells = cells;
 	}
 	
-	Set merge(Set other) {
+	//intended to be used immediately after calling constructor
+	void add(Cell c) {
+		cells[0] = c;
+	}
+	
+	void merge(Set other) {
 		int size = cells.length + other.cells.length; //find size of new array
 		Cell[] newSet = new Cell[size]; //instantiate new array
 		for (int i = 0; i < cells.length; i++) { //read in original array
 			newSet[i] = cells[i];
 		}
 		for (int i = cells.length; i < size; i++) { //read in other array
-			newSet[i] = other.cells[i];
+			newSet[i] = other.cells[i - cells.length];
 		}
-		return new Set(newSet);
+		cells = newSet;
 	}
 	
-//	boolean contains(int row, int col) { 
-//		for (int i = 0; i < rows.length; i++) { 
-//			if (rows[i] == row && cols[i] == col) { 
-//				return true; 
-//			} 
-//		} 
-//		return false; 
-//	}
+	boolean contains(Cell c) { 
+		for (int i = 0; i < cells.length; i++) { 
+			if (cells[i].equals(c)) { 
+				return true; 
+			} 
+		} 
+		return false; 
+	}
 	
 }
