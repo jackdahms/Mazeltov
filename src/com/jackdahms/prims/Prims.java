26ebec84c6913e1f823e9f3d9cc2fa9d5e139b38
jackdahms <jack.dahms@gmail.com>
2016-07-03 21:32:42 -0500

    added Prim's generator class

---
+++ src/com/jackdahms/prims/Prims.java
@@ -0,0 +1,79 @@
+package com.jackdahms.prims;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+import com.jackdahms.Generator;
+
+public class Prims extends Generator {
+
+	public void generate() {
+		ArrayList<Wall> walls = new ArrayList<Wall>();
+		
+		Consumer<Wall> addWall = wall -> {
+			if (!wall.getPassage() && !walls.contains(wall)) walls.add(wall);
+		};
+		
+		Cell[][] cells = new Cell[height][width];
+		
+		for (int i = 0; i < height; i++) {
+			for (int k = 0; k < width; k++) {
+				cells[i][k] = new Cell(i, k, this);
+			}
+		}
+		
+		Cell current = new Cell(starty, startx, this);
+		
+		do {
+			if (!current.getVisited()) {
+				int row = current.row;
+				int col = current.col;
+				
+				//adds walls to list 
+				if (row == 0) {
+					addWall.accept(new Wall(row, col, true, this));
+				} else if (row == height - 1) {
+					addWall.accept(new Wall(row - 1, col, true, this));
+				} else {
+					addWall.accept(new Wall(row, col, true, this));
+					addWall.accept(new Wall(row - 1, col, true, this));
+				}
+				
+				if (col == 0) {
+					addWall.accept(new Wall(row, col, false, this));
+				} else if (col == width - 1) {
+					addWall.accept(new Wall(row, col - 1, false, this));
+				} else {
+					addWall.accept(new Wall(row, col, false, this));
+					addWall.accept(new Wall(row, col - 1, false, this));
+				}
+			}
+			current.setVisited(true);
+			//get random wall
+			Wall wall = walls.get((int)(walls.size() * Math.random()));
+			//get the two cells it divides
+			Cell a = cells[wall.row][wall.col];
+			Cell b = wall.horizontal ? cells[wall.row + 1][wall.col] : cells[wall.row][wall.col + 1];
+			
+			if (a.getVisited() && b.getVisited()) { //if both are visited
+				walls.remove(wall);
+			} else { //if one is visited
+				//clear the wall
+				wall.setPassage(true);
+				//visit the unvisited cell
+				if (a.getVisited()) {
+					current = b;
+				} else {
+					current = a;
+				}	
+			}			
+		} while (walls.size() > 0);
+	}
+	
+	public String toString() {
+		return "Classical Prim's Algorithm";
+	}
+
+}
